c970657880cc7b4f92b960a79aa3cfe421a0d8b1
leomet07 <leomet07@gmail.com>
2024-12-01 16:57:49 -0500

    Get max mean and stdev

--- analysis/main.py
+++ analysis/main.py
@@ -1,5 +1,6 @@
 import db_utils
 import raster_utils
+import time
 
 spatial_prediction_id = input("Enter a spatial prediciton map record's id: ")
 
@@ -7,5 +8,36 @@ downloaded_raster_bytes = db_utils.download_prediction_image_by_record_id(
     spatial_prediction_id
 )
 
-max_val = raster_utils.get_max_from_predictions_raster_bytes(downloaded_raster_bytes)
+max_val, mean_val, stdev = raster_utils.get_max_from_predictions_raster_bytes(
+    downloaded_raster_bytes
+)
 print(f"Max from {spatial_prediction_id}: {max_val}µg/L")
+print(f"Mean from {spatial_prediction_id}: {mean_val}µg/L")
+print(f"STDEV from {spatial_prediction_id}: {stdev}")
+
+
+# def test():
+#     downloaded_raster_bytes, return_date = (
+#         db_utils.download_first_prediction_image_by_date_range(
+#             100965, "2017-05-31", "2020-05-31"
+#         )
+#     )
+
+#     max_val = raster_utils.get_max_from_predictions_raster_bytes(
+#         downloaded_raster_bytes
+#     )
+#     print(f"Max from {return_date}: {max_val}µg/L")
+
+
+# start_time = time.time()
+
+# test()
+
+# end_time = time.time()
+
+# elapsed_time = end_time - start_time
+
+# print(elapsed_time)
+
+
+# wfwglyjn82le9z5
--- analysis/raster_utils.py
+++ analysis/raster_utils.py
@@ -2,56 +2,45 @@ import numpy as np
 import rasterio
 
 
+def run_analytics_on_opened_raster(src):
+    # Assumes src comes from:     with rasterio.open(file_path) as src:
+
+    # Read the number of bands and the dimensions
+    num_bands = src.count  # This should be 1 for the output of the prediction file
+    if num_bands != 1:
+        raise Exception(
+            f"Number of bands in {file_path} should be 1 instead of {num_bands}. Are you sure this file is chl_a predictions from the model?"
+        )
+    height = src.height
+    width = src.width
+
+    raster_array = src.read()
+
+    max_val = raster_array.max()  # This DOES work with 2d arrays
+
+    flatten_raster_array = raster_array.flatten()
+
+    mean_val = np.mean(flatten_raster_array)
+    stdev = np.std(flatten_raster_array)
+
+    # top_ten_highest_indices = np.argpartition(flatten_raster_array, -10)[-10:]
+    # top_ten = flatten_raster_array[top_ten_highest_indices]
+    # top_ten.sort()
+    # print("Top ten: ", top_ten)
+
+    return max_val, mean_val, stdev
+
+
 def get_max_from_predictions_raster_bytes(raster: bytes) -> float:
     with rasterio.io.MemoryFile(raster) as memfile:
         with memfile.open() as src:
-            # Read the number of bands and the dimensions
-            num_bands = (
-                src.count
-            )  # This should be 1 for the output of the prediction file
-            if num_bands != 1:
-                raise Exception(
-                    f"Number of bands in file should be 1 instead of {num_bands}. Are you sure this file is chl_a predictions from the model?"
-                )
-            height = src.height
-            width = src.width
-
-            raster_array = src.read()
-
-            max_val = raster_array.max()  # This DOES work with 2d arrays
-
-            # raster_array = raster_array.flatten()
-            # top_ten_highest_indices = np.argpartition(raster_array, -10)[-10:]
-            # top_ten = raster_array[top_ten_highest_indices]
-            # top_ten.sort()
-            # print("Top ten: ", top_ten)
-
-            return max_val
+            return run_analytics_on_opened_raster(src)
 
 
 def get_max_from_predictions_raster_file(file_path: str) -> int:
     print(f"Opening {file_path}...")
     with rasterio.open(file_path) as src:
-        # Read the number of bands and the dimensions
-        num_bands = src.count  # This should be 1 for the output of the prediction file
-        if num_bands != 1:
-            raise Exception(
-                f"Number of bands in {file_path} should be 1 instead of {num_bands}. Are you sure this file is chl_a predictions from the model?"
-            )
-        height = src.height
-        width = src.width
-
-        raster_array = src.read()
-
-        max_val = raster_array.max()  # This DOES work with 2d arrays
-
-        # raster_array = raster_array.flatten()
-        # top_ten_highest_indices = np.argpartition(raster_array, -10)[-10:]
-        # top_ten = raster_array[top_ten_highest_indices]
-        # top_ten.sort()
-        # print("Top ten: ", top_ten)
-
-        return max_val
+        return run_analytics_on_opened_raster(src)
 
 
 if __name__ == "__main__":
