360bb1457ce1872efcd88268b2fad8beb58b1867
leomet07 <47071760+leomet07@users.noreply.github.com>
2025-02-08 14:05:21 -0500

    factor out model data component of cpu and gpu training

--- ml_model/model_training.py
+++ ml_model/model_training.py
@@ -16,84 +16,12 @@ import pandas as pd
 import time
 import numpy as np
 from matplotlib import pyplot as plt
+import model_data
 
-NAN_SUBSTITUTE_CONSANT = -99999
-
-# Creating the proper dataframe (csv) from other datasets
-training_df_path = os.getenv("INSITU_CHLA_TRAINING_DATA_PATH") # training data
-lagosid_path = os.getenv("CCRI_LAKES_WITH_LAGOSID_PATH") # needed to get lagoslakeid for training data entries
-lulc_path = os.getenv("LAGOS_LAKE_INFO_PATH") # General lake info (for constants)
 DO_HYPERPARAM_SEARCH = os.getenv("DO_HYPERPARAM_SEARCH").lower() == "true"
 GRAPH_AND_COMPARE_PERFORMANCE = os.getenv("GRAPH_AND_COMPARE_PERFORMANCE").lower() == "true"
 
-def prepare_data(df_path, lagosid_path, lulc_path, random_state=621, test_size=0.1):
-    # read csvs
-    df = pd.read_csv(df_path)
-
-    lagosid = pd.read_csv(lagosid_path)
-    print("CSV imported")
-
-    # select relevant columns from lagosid
-    lagosid = lagosid[['lagoslakei']]
-    df = pd.concat([lagosid, df], axis=1)
-
-    df = df[df['chl_a'] < 2000]
-
-    # create chl-a mathematical inputs
-    df["NDCI"] = ((df['703'] - df['665']) / (df['703'] + df['665']))
-    df["NDVI"] = ((df['864'] - df['665']) / (df['864'] + df['665']))
-    df["3BDA"] = (((df['493'] - df['665']) / (df['493'] + df['665'])) - df['560'])
-
-    # create rough vol based on depth & SA
-    df["lake_vol"] = (df['SA'] * df['Max.depth'])
-
-    # load lagos-ne iws data, merged with dataframe
-    iws_lulc = pd.read_csv(lulc_path)
-
-    # summarize percent developed land
-    iws_lulc['iws_nlcd2011_pct_dev'] = iws_lulc['iws_nlcd2011_pct_21'] + \
-        iws_lulc['iws_nlcd2011_pct_22'] + iws_lulc['iws_nlcd2011_pct_23'] + \
-        iws_lulc['iws_nlcd2011_pct_24']
-
-    # summarize percent agricultural land
-    iws_lulc['iws_nlcd2011_pct_ag'] = iws_lulc['iws_nlcd2011_pct_81'] + iws_lulc['iws_nlcd2011_pct_82']
-
-    # create new dataframe with the two variables
-    iws_human = iws_lulc[['lagoslakeid', 'iws_nlcd2011_pct_dev', 'iws_nlcd2011_pct_ag']]
-
-    iws_human = iws_human.rename(columns={'lagoslakeid': 'lagoslakei', 'iws_nlcd2011_pct_dev': 'pct_dev', 'iws_nlcd2011_pct_ag': 'pct_ag'})
-    # At this point, iws_human contains 50k rows (ALL lagos lakes, including the 4400 NYS lakes)
-    # print("one of 4k lakes after: ", iws_human.loc[lambda x: x['lagoslakei'] == 57171])
-    # print("# of rows in iws_human: ", len(iws_human))
-
-    # left join df with iws_human, which cuts # of lakes in df to 360 (14k entries of in situ readings)
-    df = df.merge(iws_human, on="lagoslakei")
-    return df, iws_human # Array of pandas dataframes
-
-
-def prepared_cleaned_data(unclean_data): # Returns CUDF df
-    unclean_data = unclean_data[['chl_a', '443', '493', '560', '665','703', '740', '780', '834', '864','SA','Max.depth','pct_dev','pct_ag']]
-    unclean_data = unclean_data.fillna(NAN_SUBSTITUTE_CONSANT)
-    input_cols = ['443', '493', '560', '665','703', '740', '780', '834', '864','SA','Max.depth','pct_dev','pct_ag']
-    for col in unclean_data.select_dtypes(["object"]).columns:
-        unclean_data[col] = unclean_data[col].astype("category").cat.codes.astype(np.int32)
-
-    # cast all columns to int32
-    for col in unclean_data.columns:
-        unclean_data[col] = unclean_data[col].astype(np.float32)  # needed for random forest
-
-    # put target/label column first [ classic XGBoost standard ]
-    output_cols = ["chl_a"] + input_cols
-    unclean_data.to_csv("preindexaspandas.csv")
-    unclean_data = unclean_data.reindex(columns=output_cols)
-
-    unclean_data.to_csv("postindexaspandas.csv")
-    df_cudf = cudf.from_pandas(unclean_data)
-    return df_cudf
-
-all_data, lagos_lookup_table = prepare_data(training_df_path, lagosid_path, lulc_path) # Returns insitu points merged with lagoslookup table AND lagoslookup table for all non-insitu lakes as well
-cleaned_data = prepared_cleaned_data(all_data)
-cleaned_data = cleaned_data[cleaned_data['chl_a'] < 350] # most values are 0-100, remove the crazy 4,000 outlier
+cleaned_data = cudf.from_pandas(model_data.cleaned_data)
 
 # Convert float columns to integers
 for col in cleaned_data.columns:
@@ -105,17 +33,6 @@ y = cleaned_data['chl_a']
 # Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=621)
 print("Dataframes created and data split successfully.")
-
-# define constants for new bands
-def get_constants(lakeid):
-    filtered_df = all_data[all_data['lagoslakei'] == lakeid] # maybe this lake is from insitu and we know it's depth and surface area?
-    
-    SA = filtered_df['SA'].iloc[0] if not filtered_df.empty else NAN_SUBSTITUTE_CONSANT # NAN_SUBSTITUTE_CONSANT = null in our cudf random forest
-    Max_depth = filtered_df['Max.depth'].iloc[0]  if not filtered_df.empty else NAN_SUBSTITUTE_CONSANT
-    pct_dev = lagos_lookup_table['pct_dev'].iloc[0] # Lagos look up table should have this
-    pct_ag = lagos_lookup_table['pct_ag'].iloc[0] # Lagos look up table should have this
-
-    return SA, Max_depth, pct_dev, pct_ag
 
 def hyper_param_search_and_train_model():
     n_estimators = [int(x) for x in np.linspace(start = 200, stop = 2000, num = 10)]
--- ml_model/scikit_model_training.py
+++ ml_model/scikit_model_training.py
@@ -1,3 +1,9 @@
+# Setup dotenv
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 from sklearn.metrics import r2_score
 from sklearn.metrics import mean_squared_error
 import pandas as pd
@@ -6,24 +12,22 @@ import numpy as np
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from matplotlib import pyplot as plt
+import model_data
+from scipy import stats
 
-cleaned_data = pd.read_csv("postindexaspandas_good.csv")
+GRAPH_AND_COMPARE_PERFORMANCE = os.getenv("GRAPH_AND_COMPARE_PERFORMANCE").lower() == "true"
 
+cleaned_data = model_data.cleaned_data
 # cleaned_data = cleaned_data.replace(-99999.0, np.nan) # Not needed, -99999 working fine
 
 X = cleaned_data.drop(columns=['chl_a'])
 y = cleaned_data['chl_a']
 
-print(X.head)
-# print(y)
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=621)
 print("Dataframes created and data split successfully.")
 
-
-
 andrew_params = {
-     'max_depth': 30, # Andrew params
+    'max_depth': 30, # Andrew params
     'max_features': 'sqrt',
     'min_samples_leaf' :1,
     'min_samples_split' :2,
@@ -52,34 +56,32 @@ rmse = mean_squared_error(y_test, y_pred) ** 0.5
 print(f"r2 score: {r2}")
 print(f"RMSE: {rmse}")
 
-print("mean of test: ", np.mean(y_test))
-print("mean of pred: ", np.mean(y_pred))
+
+if GRAPH_AND_COMPARE_PERFORMANCE:
+    print("mean of test: ", np.mean(y_test))
+    print("mean of pred: ", np.mean(y_pred))
+
+    # Model analysis
+    plt.figure(1)
+    plt.hist(y_pred, 200)
+    plt.xlabel("Predicted chl-a (ug/l)")
+    plt.ylabel("Frequency")
+    plt.title("Prediction Histogram (CPU)")
 
 
+    # Plot y_pred performance: https://stackoverflow.com/questions/19064772/visualization-of-scatter-plots-with-overlapping-points-in-matplotlib
+    plt.figure(2)
+    values = np.vstack([y_test, y_pred])
+    kernel = stats.gaussian_kde(values, bw_method=.02)(values)
 
-# Model analysis
-plt.figure(1)
-plt.hist(y_pred, 200)
-plt.xlabel("Predicted chl-a (ug/l)")
-plt.ylabel("Frequency")
-plt.title("Prediction Histogram (CPU)")
+    plt.scatter(y_test, y_pred, s=20, c=kernel,cmap='viridis')
+    # plt.axline((0,0), (50,50), linewidth=1, color='black')
+    # plt.axis((0,50,0,50))
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.xlabel('Observed Chl-a (ug/l)')
+    plt.ylabel('Predicted Chl-a (ug/l)')
+    plt.title('CPU Random Forest Regression')
 
-
-# PLOT BEST RF PERFORMANCE
-#https://stackoverflow.com/questions/19064772/visualization-of-scatter-plots-with-overlapping-points-in-matplotlib
-from scipy import stats
-plt.figure(2)
-values = np.vstack([y_test, y_pred])
-kernel = stats.gaussian_kde(values, bw_method=.02)(values)
-
-plt.scatter(y_test, y_pred, s=20, c=kernel,cmap='viridis')
-# plt.axline((0,0), (50,50), linewidth=1, color='black')
-# plt.axis((0,50,0,50))
-plt.xscale('log')
-plt.yscale('log')
-plt.xlabel('Observed Chl-a (ug/l)')
-plt.ylabel('Predicted Chl-a (ug/l)')
-plt.title('CPU Random Forest Regression')
-
-# Show both at the same time
-plt.show()
+    # Show both at the same time
+    plt.show()
