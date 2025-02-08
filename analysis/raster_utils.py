b882371b0f6e02afc3aa47f5a1721804fdcbddcc
leomet07 <47071760+leomet07@users.noreply.github.com>
2025-02-08 11:16:10 -0500

    refactor to use raster array as value to pass around util funcs

--- analysis/raster_utils.py
+++ analysis/raster_utils.py
@@ -1,21 +1,15 @@
 import numpy as np
 import rasterio
 
-
-def run_analytics_on_opened_raster(src):
-    # Assumes src comes from:     with rasterio.open(file_path) as src:
-
+def check_src(src):
     # Read the number of bands and the dimensions
     num_bands = src.count  # This should be 1 for the output of the prediction file
     if num_bands != 1:
         raise Exception(
             f"Number of bands should be 1 instead of {num_bands}. Are you sure this file is chl_a predictions from the model?"
         )
-    height = src.height
-    width = src.width
 
-    raster_array = src.read()
-
+def run_analytics_on_raster(raster_array):
     flatten_raster_array = raster_array.flatten()
 
     max_val = np.nanmax(flatten_raster_array)
@@ -32,15 +26,25 @@ def run_analytics_on_opened_raster(src):
     return max_val, min_val, mean_val, stdev
 
 
-def get_max_from_predictions_raster_bytes(raster: bytes) -> float:
-    with rasterio.io.MemoryFile(raster) as memfile:
+def get_raster_array_from_file(file_path: str):
+    with rasterio.open(file_path) as src:
+        check_src(src)
+        return src.read()
+
+def get_raster_array_from_bytes(raster_bytes: bytes):
+    with rasterio.io.MemoryFile(raster_bytes) as memfile:
         with memfile.open() as src:
-            return run_analytics_on_opened_raster(src)
+            check_src(src)
+            return src.read()
+
+def get_max_from_predictions_raster_bytes(raster_bytes: bytes) -> float:
+    raster_array = get_raster_array_from_bytes(raster_bytes)
+    return run_analytics_on_raster(raster_array)
 
 
 def get_max_from_predictions_raster_file(file_path: str) -> int:
-    with rasterio.open(file_path) as src:
-        return run_analytics_on_opened_raster(src)
+    raster_array = get_raster_array_from_file(file_path)
+    return run_analytics_on_raster(raster_array)
 
 
 if __name__ == "__main__":
