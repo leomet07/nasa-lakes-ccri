605f0c84e8d8c789918dff6a4506110663b583f0
leomet07 <47071760+leomet07@users.noreply.github.com>
2025-03-25 15:46:41 -0400

    fixed xaxis for feature importance

--- ml_model/cpu_model_training.py
+++ ml_model/cpu_model_training.py
@@ -26,7 +26,9 @@ CPU_MODEL_SAVE_FILE = "model_cpu.joblib"
 
 import model_data
 training_data = model_data.training_data
-# cleaned_data = cleaned_data.replace(-99999.0, np.nan) # Not needed, -99999 working fine
+# training_data = training_data.replace(-99999.0, np.nan) # Not needed, -99999 working fine
+# training_data = training_data.replace(-99999.0, -1.0) # Not needed, -99999 working fine
+# training_data = training_data.replace(-99999.0, -9999999.0) # Not needed, -99999 working fine
 
 X = training_data.drop(columns=['chl_a'])
 y = training_data['chl_a']
@@ -193,6 +195,7 @@ if GRAPH_AND_COMPARE_PERFORMANCE:
     # plt.barh(range(len(indices)), feature_importances[indices], color='b', align='center') # Sorted
     # plt.yticks(range(len(indices)), [features[i] for i in indices])
     plt.barh(features, feature_importances, color="skyblue", ec="black")
+    plt.xlim(0, 0.3)
     plt.xlabel('Relative Importance', fontweight='bold')
     # plt.yticks(range(len(indices)), [features[i] for i in indices])
     plt.tight_layout()
