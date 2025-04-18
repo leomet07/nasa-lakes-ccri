1e79b5c4968402bad83d82e0ce77e8d0736c38bf
leomet07 <leomet07@gmail.com>
2025-04-18 13:06:05 -0400

    use mongo for lakes sorted by number of spatial predictions

--- analysis/lakes_sorted_by_number_of_spatial_predictions.py
+++ analysis/lakes_sorted_by_number_of_spatial_predictions.py
@@ -1,12 +1,34 @@
 import db_utils
 from pprint import pprint
+from tqdm import tqdm
 
-lakes = db_utils.client.collection("lakes").get_full_list(batch=100_000, query_params={"expand" : "spatial_predictions"})
+occurances_dict = {}
 
-pprint(vars(lakes[1]))
-lakes.sort(key=lambda x: len(x.spatial_predictions))
+all_spatial_predictions_list = list(
+    db_utils.spatial_predictions_collection.find({})
+)  # query all then filter, faster than searching and querying on db level
 
-for lake in lakes:
+for spatial_prediction in all_spatial_predictions_list:
+    lagoslakeid = spatial_prediction["lagoslakeid"]
+    if lagoslakeid not in occurances_dict:
+        occurances_dict[lagoslakeid] = 1
+    else:
+        occurances_dict[lagoslakeid] += 1
+
+occurances_as_list = list(occurances_dict.items())
+
+occurances_as_list.sort(
+    key=lambda x: x[
+        1
+    ],  # sort on the second part of tuple, aka the number of occurances
+    reverse=True,
+)
+
+for pair in occurances_as_list[:10]:  # top ten occurances
+    lagoslakeid, num_occurances = pair
+
+    matched_lake = db_utils.lakes_collection.find_one({"lagoslakeid": lagoslakeid})
+
     print(
-        f"{lake.name}, lagoslakeid: {lake.lagoslakeid}, # of predictions: {len(lake.spatial_predictions)}"
+        f"{matched_lake["name"]} (lagoslakeid - {lagoslakeid}): {num_occurances} occurances"
     )
