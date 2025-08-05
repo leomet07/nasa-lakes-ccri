371100ddcae4d5ad2959afb2ebfcd03fc1a20c65
leomet07 <47071760+leomet07@users.noreply.github.com>
2025-08-05 16:23:32 -0400

    add satellite and image index to uploads+ lint

--- ml_model/lenny_fall_run_all_workbook.py
+++ ml_model/lenny_fall_run_all_workbook.py
@@ -47,14 +47,14 @@ session_uuid = str(uuid.uuid4())
 print("Current session id: ", session_uuid)
 
 
-input_tif_folder = os.getenv("INPUT_TIF_FOLDER") # Specify the folder inside of the tar
+input_tif_folder = os.getenv("INPUT_TIF_FOLDER")  # Specify the folder inside of the tar
 paths = os.listdir(input_tif_folder)
 print("Number of files to run: ", len(paths))
 
 png_out_folder = os.path.join("all_png_out", f"png_out_{session_uuid}")
 if not os.path.exists(png_out_folder):
     os.makedirs(png_out_folder)
-    
+
 tif_out_folder = os.path.join("all_tif_out", f"tif_out_{session_uuid}")
 if not os.path.exists(tif_out_folder):
     os.makedirs(tif_out_folder)
@@ -66,29 +66,38 @@ if not os.path.exists(session_statues_path):
 error_paths = []
 
 
-def add_suffix_to_filename_at_tif_path(filename : str, suffix : str):
+def add_suffix_to_filename_at_tif_path(filename: str, suffix: str):
     parts = filename.split(".")
-    newfilename =  parts[0] + f"_{suffix}." + ".".join(parts[1:])
+    newfilename = parts[0] + f"_{suffix}." + ".".join(parts[1:])
 
     to_tif_folder_path = os.path.join(tif_out_folder, os.path.basename(newfilename))
 
     return to_tif_folder_path
 
 
-def modify_tif(input_tif : str, SA_SQ_KM_FROM_SHAPEFILE_constant : float, pct_dev_constant: float, pct_ag_constant : float) -> str:
+def modify_tif(
+    input_tif: str,
+    SA_SQ_KM_FROM_SHAPEFILE_constant: float,
+    pct_dev_constant: float,
+    pct_ag_constant: float,
+) -> str:
     with rasterio.open(input_tif) as src:
         raster_data = src.read()
         profile = src.profile  # Get the profile of the existing raster
         tags = src.tags()
-    
+
     # print(tags)
     satellite = tags["satellite"]
     # print("satellite: ", satellite)
 
     # create new bands
-    SA_SQ_KM_band = np.full_like(raster_data[0], SA_SQ_KM_FROM_SHAPEFILE_constant, dtype=raster_data.dtype)
+    SA_SQ_KM_band = np.full_like(
+        raster_data[0], SA_SQ_KM_FROM_SHAPEFILE_constant, dtype=raster_data.dtype
+    )
     # Max_depth_band = np.full_like(raster_data[0], Max_depth_constant, dtype=raster_data.dtype)
-    pct_dev_band = np.full_like(raster_data[0], pct_dev_constant, dtype=raster_data.dtype)
+    pct_dev_band = np.full_like(
+        raster_data[0], pct_dev_constant, dtype=raster_data.dtype
+    )
     pct_ag_band = np.full_like(raster_data[0], pct_ag_constant, dtype=raster_data.dtype)
 
     # update profile to reflect additional bands
@@ -99,19 +108,25 @@ def modify_tif(input_tif : str, SA_SQ_KM_FROM_SHAPEFILE_constant : float, pct_de
     if satellite.startswith("sentinel"):
         bands_to_fill = 0
     elif satellite.startswith("landsat"):
-        bands_to_fill = 9 - 5 # Landsat has 5, not 9 bands, so fill 4 bands
+        bands_to_fill = 9 - 5  # Landsat has 5, not 9 bands, so fill 4 bands
     else:
         raise Exception(f'Satellite "{satellite}" predictions not implemented yet.')
-    
-    with rasterio.open(modified_tif, 'w', **profile) as dst:
+
+    with rasterio.open(modified_tif, "w", **profile) as dst:
         # write original bands
         for i in range(1, raster_data.shape[0] + 1):
-            dst.write(raster_data[i-1], indexes=i)
+            dst.write(raster_data[i - 1], indexes=i)
 
-        bands_to_fill = 9 - 5 
-        for i in range(raster_data.shape[0] + 1, raster_data.shape[0] + 1 + bands_to_fill):
+        bands_to_fill = 9 - 5
+        for i in range(
+            raster_data.shape[0] + 1, raster_data.shape[0] + 1 + bands_to_fill
+        ):
             # print(f"Writing null band... at ({i})")
-            null_band = np.full_like(raster_data[0], model_data.NAN_SUBSTITUTE_CONSANT, dtype=raster_data.dtype)
+            null_band = np.full_like(
+                raster_data[0],
+                model_data.NAN_SUBSTITUTE_CONSANT,
+                dtype=raster_data.dtype,
+            )
             dst.write(null_band, indexes=i)
 
         # # write additional bands
@@ -126,7 +141,7 @@ def modify_tif(input_tif : str, SA_SQ_KM_FROM_SHAPEFILE_constant : float, pct_de
     return modified_tif
 
 
-def predict(input_tif : str, lakeid: int, tags, display = True):
+def predict(input_tif: str, lakeid: int, tags, display=True):
     modified_tif = add_suffix_to_filename_at_tif_path(input_tif, "modified")
     with rasterio.open(modified_tif) as src:
         raster_data = src.read()
@@ -134,11 +149,15 @@ def predict(input_tif : str, lakeid: int, tags, display = True):
 
     n_bands, n_rows, n_cols = raster_data.shape
     n_samples = n_rows * n_cols
-    raster_data_2d = raster_data.transpose(1, 2, 0).reshape((n_samples,n_bands))
+    raster_data_2d = raster_data.transpose(1, 2, 0).reshape((n_samples, n_bands))
 
-    non_finite_val_mask = ~np.isfinite(raster_data[0]) # if first band at that pixel is nan, inf, or -inf, usually rest are too (helps remove "garbage" val from output later)
-    
-    raster_data_2d[~np.isfinite(raster_data_2d)] = model_data.NAN_SUBSTITUTE_CONSANT # Replace with NAN_SUB_CONSTANT or mean of general, but this pixels output  will later be removed anyway
+    non_finite_val_mask = ~np.isfinite(
+        raster_data[0]
+    )  # if first band at that pixel is nan, inf, or -inf, usually rest are too (helps remove "garbage" val from output later)
+
+    raster_data_2d[~np.isfinite(raster_data_2d)] = (
+        model_data.NAN_SUBSTITUTE_CONSANT
+    )  # Replace with NAN_SUB_CONSTANT or mean of general, but this pixels output  will later be removed anyway
 
     # perform the prediction
     predictions = andrew_model.predict(raster_data_2d)
@@ -148,16 +167,20 @@ def predict(input_tif : str, lakeid: int, tags, display = True):
         df["lagoslakeid"] = lakeid
         df["pred"] = predictions
         df = df.drop_duplicates()
-        output_tif_csv = add_suffix_to_filename_at_tif_path(input_tif, "predicted") + '.csv'
+        output_tif_csv = (
+            add_suffix_to_filename_at_tif_path(input_tif, "predicted") + ".csv"
+        )
         df.to_csv(output_tif_csv)
-        print("csv saved to " +  os.path.join(os.getcwd(), output_tif_csv))
+        print("csv saved to " + os.path.join(os.getcwd(), output_tif_csv))
 
     # print(predictions)
 
     # reshape the predictions back to the original raster shape
     predictions_raster = predictions.reshape(n_rows, n_cols)
 
-    predictions_raster[non_finite_val_mask] = np.nan # if the input value was originally nan, -inf, or, ignore its (normal-seeming) output and make it nan
+    predictions_raster[non_finite_val_mask] = (
+        np.nan
+    )  # if the input value was originally nan, -inf, or, ignore its (normal-seeming) output and make it nan
 
     if not IS_IN_PRODUCTION_MODE:
         print("Min predictions: ", np.nanmin(predictions_raster))
@@ -168,16 +191,18 @@ def predict(input_tif : str, lakeid: int, tags, display = True):
     # save the prediction result as a new raster file
     output_tif = add_suffix_to_filename_at_tif_path(input_tif, "predicted")
 
-    profile.update(count=1) # only 1 output band, chl_a concentration!
-    with rasterio.open(output_tif, 'w', **profile) as dst:
-        dst.write(predictions_raster, 1) # write to band 1
+    profile.update(count=1)  # only 1 output band, chl_a concentration!
+    with rasterio.open(output_tif, "w", **profile) as dst:
+        dst.write(predictions_raster, 1)  # write to band 1
         dst.update_tags(**tags)
 
     # plot the result
     if display:
         min_cbar_value = 0
         max_cbar_value = 60
-        plt.imshow(predictions_raster, cmap='viridis', vmin=min_cbar_value, vmax=max_cbar_value)
+        plt.imshow(
+            predictions_raster, cmap="viridis", vmin=min_cbar_value, vmax=max_cbar_value
+        )
         plt.colorbar()
         plt.title(f"Predicted values for lake{lakeid} on {tags["date"]}")
         plt.show()
@@ -192,8 +217,14 @@ def save_png(input_tif, out_folder, predictions_raster, date, scale, display=Tru
     increment = 5
 
     fig = plt.figure(figsize=(10, 8))
-    plt.imshow(predictions_raster, cmap='viridis', interpolation='none', vmin=min_value, vmax=max_value)
-    plt.axis('off')
+    plt.imshow(
+        predictions_raster,
+        cmap="viridis",
+        interpolation="none",
+        vmin=min_value,
+        vmax=max_value,
+    )
+    plt.axis("off")
     stem = Path(input_tif).stem
 
     # values = np.arange(min_value, max_value + increment, increment)
@@ -206,7 +237,13 @@ def save_png(input_tif, out_folder, predictions_raster, date, scale, display=Tru
     output_png = stem + ".png"
     output_png_path = os.path.join(out_folder, output_png)
     # save the png
-    plt.savefig(output_png_path, format='png', bbox_inches='tight', pad_inches=0, transparent=True)
+    plt.savefig(
+        output_png_path,
+        format="png",
+        bbox_inches="tight",
+        pad_inches=0,
+        transparent=True,
+    )
     if display:
         plt.show()
     else:
@@ -214,7 +251,16 @@ def save_png(input_tif, out_folder, predictions_raster, date, scale, display=Tru
     return output_png_path
 
 
-def upload_spatial_map(lakeid : int, raster_image_path: str, display_image_path : str, datestr : str, corners : list, scale : int):
+def upload_spatial_map(
+    lakeid: int,
+    raster_image_path: str,
+    display_image_path: str,
+    datestr: str,
+    corners: list,
+    scale: int,
+    image_index: str,
+    satellite : str
+):
     # Step 1: Find Lakeid
 
     filtered_list = list(filter(lambda lake: lake["lagoslakeid"] == lakeid, all_lakes))
@@ -223,23 +269,26 @@ def upload_spatial_map(lakeid : int, raster_image_path: str, display_image_path 
 
     lake_db_id = filtered_list[0]["_id"]
 
-    date_obj = datetime.strptime(datestr, '%Y-%m-%d')
+    date_obj = datetime.strptime(datestr, "%Y-%m-%d")
     # Step 2
 
     body = {
-        "raster_image" :  os.path.basename(raster_image_path),
-        "display_image" : os.path.basename(display_image_path),
-        "date" : date_obj, # datetime (-> mongodate bson) type
+        "raster_image": os.path.basename(raster_image_path),
+        "display_image": os.path.basename(display_image_path),
+        "date": date_obj,  # datetime (-> mongodate bson) type
         "corner1latitude": corners[0][0],
         "corner1longitude": corners[0][1],
         "corner2latitude": corners[1][0],
         "corner2longitude": corners[1][1],
-        "scale" : scale,
-        "session_uuid" : session_uuid,
-        "lake" : lake_db_id, # bson type
-        "lagoslakeid" : lakeid
+        "scale": scale,
+        "session_uuid": session_uuid,
+        "lake": lake_db_id,  # bson type
+        "lagoslakeid": lakeid,
+        "image_index": image_index,
+        "satellite" : satellite
     }
     mongo_spatial_predictions_collection.insert_one(body)
+
 
 for path_tif in tqdm(paths):
     path_tif = os.path.join(input_tif_folder, path_tif)
@@ -248,8 +297,10 @@ for path_tif in tqdm(paths):
         with rasterio.open(path_tif) as raster:
             tags = raster.tags()
             id = int(tags["id"])
-            date = tags["date"] # date does NOT do anything here, just for title
-            scale = tags["scale"] # scale does NOT do anything here, just for title
+            date = tags["date"]  # date does NOT do anything here, just for title
+            scale = tags["scale"]  # scale does NOT do anything here, just for title
+            image_index = tags["image_index"]
+            satellite = tags["satellite"]
 
             top_left = raster.transform * (0, 0)
             bottom_right = raster.transform * (raster.width, raster.height)
@@ -263,25 +314,38 @@ for path_tif in tqdm(paths):
         # print("id: ", id, " date: ", date, " scale: ", scale, " corners: ", corners)
 
         # Get constants
-        SA_SQ_KM_constant, pct_dev_constant, pct_ag_constant = model_data.get_constants(id)
+        SA_SQ_KM_constant, pct_dev_constant, pct_ag_constant = model_data.get_constants(
+            id
+        )
         # print(f"Constants based on id({id}): ", SA_SQ_KM_constant, pct_dev_constant, pct_ag_constant)
 
-        modified_path_tif = modify_tif(path_tif, SA_SQ_KM_constant, pct_dev_constant, pct_ag_constant)
+        modified_path_tif = modify_tif(
+            path_tif, SA_SQ_KM_constant, pct_dev_constant, pct_ag_constant
+        )
 
-        output_tif, predictions_loop = predict(path_tif, id, tags, display = VISUALIZE_PREDICTIONS)
+        output_tif, predictions_loop = predict(
+            path_tif, id, tags, display=VISUALIZE_PREDICTIONS
+        )
 
         if VISUALIZE_PREDICTIONS:
-            print("Output tif: ", os.path.join(os.getcwd(),output_tif))
+            print("Output tif: ", os.path.join(os.getcwd(), output_tif))
 
-        output_path_png = save_png(path_tif, png_out_folder, predictions_loop, date, scale, display = False)
-        
+        output_path_png = save_png(
+            path_tif, png_out_folder, predictions_loop, date, scale, display=False
+        )
+
         if IS_IN_PRODUCTION_MODE:
-            upload_spatial_map(id, output_tif, output_path_png, date, corners, scale)
+            upload_spatial_map(
+                id, output_tif, output_path_png, date, corners, scale, image_index, satellite
+            )
 
-        with open(os.path.join(session_statues_path, f"successes_{session_uuid}.status.txt"), "a") as file_obj:
-            file_obj.write(path_tif +"\n")
+        with open(
+            os.path.join(session_statues_path, f"successes_{session_uuid}.status.txt"),
+            "a",
+        ) as file_obj:
+            file_obj.write(path_tif + "\n")
 
-        gc.collect() # Clear memory after prediction
+        gc.collect()  # Clear memory after prediction
     except Exception as e:
         print("Error: ", e)
         error_paths.append(path_tif)
@@ -289,5 +353,7 @@ for path_tif in tqdm(paths):
 print(f"Successfully finished {len(paths)} uploads with {len(error_paths)} errors")
 print("Session ID: ", session_uuid)
 
-with open(os.path.join(session_statues_path, f"error_paths_{session_uuid}.json"), "w") as file:
+with open(
+    os.path.join(session_statues_path, f"error_paths_{session_uuid}.json"), "w"
+) as file:
     file.write(json.dumps(error_paths))
